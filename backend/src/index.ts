0f6d8fd713ca2a581efb2b1232c9aeebe9e43547
charuagr <agrcharu94@gmail.com>
2022-09-24 18:41:11 +0200

    Added API for fetching all cities

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ const app: Express = express();
 const port = 1200;
 
 const db = new Database('../db/main.db')
-function read_database(sql:string) {
+function read_database(sql:string):Promise<Array<any>> {
     return new Promise((resolve, reject) => {
         db.all(sql, (err,rows) => {
             if(err) reject(err)
@@ -13,6 +13,16 @@ function read_database(sql:string) {
         })
     })
 }
+
+app.get('/cities', async (req: Request, res: Response) => {
+    let response:Array<string> = []
+    let data:Array<any> = await read_database("select ADM3_EN from Iraq")
+    for (const x of data) {
+        response.push(x['ADM3_EN'])
+    }
+    
+    res.send(response);
+})
 app.get('/corn', async (req: Request, res: Response) => {
     
     let data = await read_database("select ADM3_EN,\"Corn Production\" from Iraq")

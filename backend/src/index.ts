e73751ad9dd40ec3e07f6456a119111fe980cda6
charuagr <agrcharu94@gmail.com>
2022-09-24 19:38:49 +0200

    Added API for fetching all available parameters

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,9 @@
 import express, { Express, Request, Response } from 'express';
 import {Database} from 'sqlite3';
-
+import cors from'cors'
 const app: Express = express();
 const port = 1200;
-
+app.use(cors())
 const db = new Database('../db/main.db')
 function read_database(sql:string):Promise<Array<any>> {
     return new Promise((resolve, reject) => {
@@ -13,7 +13,17 @@ function read_database(sql:string):Promise<Array<any>> {
         })
     })
 }
+app.get('/params', async (req:Request, res:Response) => {
+    let response:Array<string> = []
+    let data:Array<any> = await read_database("PRAGMA table_info(Iraq)")
+    
+    for (const x of data) {
+        if (x['name'] == "ADM3_EN" || x['name'] == 'geometry') continue
+        response.push(x['name'])
+    }
+    res.send(response);
 
+})
 app.get('/cities', async (req: Request, res: Response) => {
     let response:Array<string> = []
     let data:Array<any> = await read_database("select ADM3_EN from Iraq")

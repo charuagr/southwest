930e8632f88657be6be6f1de18b11c8af270d9d8
charuagr <agrcharu94@gmail.com>
2022-09-24 17:10:22 +0200

    Simple corn backend connected with db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,25 @@
 import express, { Express, Request, Response } from 'express';
-
+import {Database} from 'sqlite3';
 
 const app: Express = express();
 const port = 1200;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+const db = new Database('../db/main.db')
+function read_database(sql:string) {
+    return new Promise((resolve, reject) => {
+        db.all(sql, (err,rows) => {
+            if(err) reject(err)
+            resolve(rows)
+        })
+    })
+}
+app.get('/corn', async (req: Request, res: Response) => {
+    
+    let data = await read_database("select ADM3_EN,\"Corn Production\" from Iraq")
+    res.send(data);
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
+
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });

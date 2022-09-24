babecd1e63c60a54e0110b2a2b351a7f0f0cea44
charuagr <agrcharu94@gmail.com>
2022-09-24 19:39:02 +0200

    Made frontend for selecting counties and parameters

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,84 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { render } from '@testing-library/react';
+import Select from 'react-select';
+
+class TableItem extends React.Component<{name:string}> {
+  render() {
+    return <option> {this.props.name}</option>
+  }
+}
+class Cities extends React.Component<{country:string}, {list:Array<string>}> {
+  constructor(props:{country:string}) {
+    super(props);
+    this.state = {
+      'list':[]
+    }
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:1200/cities").then((res:any)=>{
+      
+      this.setState({list:res.data})
+    })
+  }
+
+  render() {
+    let multiOptions = []
+    for (const x of this.state.list) {
+      multiOptions.push( {value: x, label:x})
+    }
+    return(
+      <div>
+        Select Counties you want to see
+      <Select options = {multiOptions} isMulti = {true}/>
+
+      </div>
+    );
+  }
+}
+
+
+class Params extends React.Component<{}, {list:Array<string>}> {
+  constructor(props:{country:string}) {
+    super(props);
+    this.state = {
+      'list':[]
+    }
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:1200/params").then((res:any)=>{
+      
+      this.setState({list:res.data})
+    })
+  }
+
+  render() {
+    let multiOptions = []
+    for (const x of this.state.list) {
+      multiOptions.push( {value: x, label:x})
+    }
+    return(
+      <div>
+        Select Parameters you wish to see
+      <Select options = {multiOptions} isMulti = {true}/>
+      </div>
+    );
+  }
+}
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <Cities country = "Iraq"></Cities>
+        <Params></Params>
     </div>
   );
 }
